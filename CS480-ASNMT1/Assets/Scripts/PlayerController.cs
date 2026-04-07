be23d12fa72d7f41ef7c1f7eab097eeaa8200dcf
2short2hit <twoshorttoohit@gmail.com>
2026-04-07 14:49:25 -0700

    Added double jump capability, and a jump count, with a max of 2 to limit to only 2 jumps per collision.

---
+++ CS480-ASNMT1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody _playerBody;
+    private int _score;
+    private bool _jump;
+    private int _jumpCount;
+    private const int MaxJumps = 2;
+    private bool _isGrounded;
+
+
+    void Start() {
+        _playerBody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+            if (_jumpCount < MaxJumps) {
+                _jump = true;
+                _jumpCount++;
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        float x = Keyboard.current.dKey.isPressed ? 1.0f : (Keyboard.current.aKey.isPressed ? -1.0f : 0.0f);
+        float z = Keyboard.current.wKey.isPressed ? 1.0f : (Keyboard.current.sKey.isPressed ? -1.0f : 0.0f);
+        float y = 0.0f;
+        if (_jump) {
+            y = 100.0f;
+            _jump = false;
+        }
+
+        Vector3 move = new Vector3 (x, y, z);
+        _playerBody.AddForce (move * speed);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag ("Pickup")) {
+            other.gameObject.SetActive (false);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        _jumpCount = 0;
+        _isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collision) {
+        _isGrounded = false;
+    }
+
+}
